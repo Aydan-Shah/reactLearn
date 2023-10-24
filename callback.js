f88010f00fe16c3ffd7c84a12cea25558b376f67
Aydan <aydanhilal5@gamil.com>
2023-10-24 19:36:57 +0400

    axios, promise

--- callback.js
+++ callback.js
@@ -40,11 +40,57 @@
 
 // async await
 
-const getApiData =  async () => {
-    const getProducts = await (await fetch("https://fakestoreapi.com/products")).json();
-    const getUsers = await (await fetch("https://fakestoreapi.com/users")).json();
-    console.log(getProducts)
-    console.log("********************")
-    console.log(getUsers)
+// const getApiData =  async () => {
+//     const getProducts = await (await fetch("https://fakestoreapi.com/products")).json();
+//     const getUsers = await (await fetch("https://fakestoreapi.com/users")).json();
+//     console.log(getProducts)
+//     console.log("********************")
+//     console.log(getUsers)
+// }
+// getApiData()
+
+// ***********************************
+
+// axios
+import axios from "axios";
+// const getApiData =  async () => {
+//     const {data : getProducts} = await axios("https://fakestoreapi.com/products");
+//     const {data : getUsers} = await axios ("https://fakestoreapi.com/users");
+//     console.log(getProducts)
+//     console.log("********************");
+//     console.log(getUsers)
+
+// }
+// getApiData()
+
+// ***********************************
+
+// Promise
+
+// const getProducts = (statusCode) => {
+//     return new Promise((resolve, reject) => {
+//         // console.log("List of products")
+//         if(statusCode === 200) {
+//             resolve("List of products")
+//         } else {
+//             reject("Error")
+//         }
+//     })
+// }
+// getProducts(500).then(response => console.log(response)).catch(error=> console.log(error));
+
+
+const getProducts = (statusCode) => {
+    return new Promise(async(resolve, reject) => {
+        // console.log("List of products")
+        if(statusCode === 200) {
+            const {data} = await axios("https://fakestoreapi.com/products")
+            resolve(data)
+        } else {
+            reject("Error")
+        }
+    })
 }
-getApiData()
+getProducts(200).then(response => console.log(response)).catch(error=> console.log(error));
+// const products = await getProducts(200)
+// console.log(products)
